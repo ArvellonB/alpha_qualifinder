9d958ebab8fc83b544921c3bdef6beaeceeb3f4f
Dan <dantonnewman@gmail.com>
2024-05-09 02:46:15 -0700

    Delete connections directory

--- connections/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Connection
-
-# View for user connections
-def user_connections(request, user_id):
-    connections = Connection.objects.filter(user1_id=user_id)
-    return render(request, 'connections/connections.html', {'connections': connections})
